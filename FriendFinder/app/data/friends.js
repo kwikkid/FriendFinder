9d184ebffe99ede427d1643fc0037436fba9ef30
lynne.kwik@gmail.com <lynne.kwik@gmail.com>
2019-06-14 21:57:15 -0700

    updates to api routes

--- FriendFinder/app/data/friends.js
+++ FriendFinder/app/data/friends.js
@@ -1,36 +1,36 @@
 // //store application's data inside app/data/friends.js as an array of objects //
 
-// var friendsArray = [{
-//         name: "Marutaro",
-//         image: "#",
-//         scores: []
+var friendsArray = [
+	{
+		name: "Marutaro",
+		image: "#",
+		scores: [3, 2, 3, 4, 2, 2, 3, 4, 4, 4, 1, 1]
+	},
+	{
+		name: "Euro",
+		image: "#",
+		scores: [2, 5, 1, 5, 3, 3, 5, 3, 4, 5, 4, 4]
+	},
+	{
+		name: "BunCha",
+		image: "#",
+		scores: [4, 5, 2, 4, 5, 4, 5, 1, 4, 4, 3, 1]
+	},
+	{
+		name: "Nala",
+		image: "#",
+		scores: [4, 2, 3, 2, 5, 5, 2, 2, 1, 1, 3]
+	},
+	{
+		name: "Grumpy Cat",
+		image: "#",
+		scores: [1, 1, 3, 2, 2, 1, 2, 2, 1, 1, 2, 4]
+	},
+	{
+		name: "Doge",
+		image: "#",
+		scores: [4, 3, 2, 2, 3, 2, 2, 3, 4, 3, 3, 1]
+	}
+];
 
-//     },
-//     {
-//         name: "Euro",
-//         image: "#",
-//         scores: []
-//     },
-//     {
-//     	name: "BunCha",
-//     	image: "#",
-//     	scores: []
-//     },
-//     {
-//     	name: "Nala",
-//     	image: "#",
-//     	scores: []
-//     },
-//     {
-//     	name: "Grumpy Cat",
-//     	image: "#",
-//     	scores: []
-//     },
-//     {
-//     	name: "Doge",
-//     	image: "#",
-//     	scores: []
-//     }
-// ]
-
-// module.exports = friendsArray;
+module.exports = friendsArray;
--- FriendFinder/app/routing/apiRoutes.js
+++ FriendFinder/app/routing/apiRoutes.js
@@ -4,12 +4,37 @@ var friendsData = require("../data/friends.js");
 
 module.exports = function(app) {
 	app.get("/api/friends", function(req, res) {
-		res.json(friensData);
+		res.json(friendsData);
 	});
 
-	// app.post("/api/friends", function(req, res){
+	app.post("/api/friends", function(req, res) {
+		var currentUser = req.body;
+		currentUser.routeName = currentUser.name
+			.replace(/\s+/g, "")
+			.toLowerCase();
+		console.log(currentUser);
 
-	// })
+		for (var i = 0; i < currentUser.scores.length; i++) {
+			var userScore = currentUser.scores[i];
+			console.log("user score " + userScore);
+		}
+
+		for (var i = 0; i < friendsData.length; i++) {
+			var dbScore = friendsData[i].scores[i];
+			console.log("dbScore" + dbScore);
+			// 	console.log("dbScore: " + dbSCore)
+			// 	// var difference = userScore - dbScore;
+			// 	// console.log(difference);
+			// }
+
+			friendsData.push(currentUser);
+
+			// var bestMatch ={
+			// 	name: "",
+			// 	image: "",
+			// 	scores:
+		}
+	});
 };
 //create two routes//
 // GET route with the url "/ api/friends"  and POST route with "/api/friends"
