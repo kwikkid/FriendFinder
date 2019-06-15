fb06e5e0c9248250815f469a6b091b1ba406f7d5
lynne.kwik@gmail.com <lynne.kwik@gmail.com>
2019-06-14 21:59:34 -0700

    added additional ports

--- FriendFinder/server.js
+++ FriendFinder/server.js
@@ -2,7 +2,7 @@ var express = require("express");
 var path = require("path");
 
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -19,3 +19,4 @@ require("./app/routing/htmlRoutes.js")(app);
 app.listen(PORT, function() {
 	console.log("App listening on PORT " + PORT);
 });
+
